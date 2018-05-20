c2923022f933d50a4b6d0a9498dfabc564e17af4
robert.ahmerov <robert.ahmerov@lamoda.ru>
2018-05-20 19:46:10 +0300

    Add docker-machine-ipconfig.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# brew
-My brew formulas
+# Zored Brew
+My brew formulas are here.
+- `docker-machine-ipconfig`.
---
+++ docker-machine-ipconfig.rb
@@ -0,0 +1,16 @@
+class DockerMachineIpconfig < Formula
+  desc "Manipulate docker-machine network interface settings"
+  homepage "https://github.com/fivestars/docker-machine-ipconfig"
+  url "https://github.com/fivestars/docker-machine-ipconfig.git",
+    :tag => "v1.0.0"
+
+  depends_on "docker-machine" => :recommended
+
+  def install
+    bin.install "docker-machine-ipconfig"
+  end
+
+  test do
+    assert_match "Usage: docker-machine-ipconfig", shell_output(bin/"docker-machine-ipconfig --help")
+  end
+end
